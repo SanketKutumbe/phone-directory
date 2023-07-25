85ae32abede280264fd544168ce053ebbd5a3541
SanketKutumbe <kutumbe.sanket@gmail.com>
2023-07-25 17:26:01 +0530

    Added test case for Addition, deletion and updation of contact.

--- src/test/java/io/sanket/directory/phone/phonedirectory/controller/ContactControllerTest.java
+++ src/test/java/io/sanket/directory/phone/phonedirectory/controller/ContactControllerTest.java
@@ -1,6 +1,7 @@
 package io.sanket.directory.phone.phonedirectory.controller;
 
 import io.sanket.directory.phone.phonedirectory.entity.Contact;
+import io.sanket.directory.phone.phonedirectory.exception.ContactAdditionFailedException;
 import io.sanket.directory.phone.phonedirectory.exception.InsufficientAttributeProvisionException;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,21 @@ class ContactControllerTest {
     private ContactController contactController = new ContactController();
 
     @Test
-    public void addContactTest() throws InsufficientAttributeProvisionException{
+    public void addContactTest() throws ContactAdditionFailedException, InsufficientAttributeProvisionException{
+        Contact contact = new Contact("aaa","111","aaa@gmail.com");
+        assertEquals(201, contactController.addContact(contact).getStatus());
         assertEquals(400, contactController.addContact(new Contact()).getStatus());
+    }
+
+    @Test
+    public void updateContactTest() throws InsufficientAttributeProvisionException{
+        Contact contact = new Contact();
+        contact.setPhoneNumber("111");
+        assertEquals(400, contactController.updateContact(contact,"1").getStatus());
+    }
+
+    @Test
+    public void deleteContactTest() throws InsufficientAttributeProvisionException{
+        assertEquals(204, contactController.deleteContact(1).getStatus());
     }
 }
